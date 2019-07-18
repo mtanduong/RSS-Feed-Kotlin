b411457a3b98d990870685b7fabf8c04a151835f
Mike Duong <mtanduong@gmail.com>
2019-07-17 20:52:14 -0700

    Implemented parse logic to parse xml data in ParseApplications

--- app/src/main/java/com/mtanduong/rssfeedapp/ParseApplications.kt
+++ app/src/main/java/com/mtanduong/rssfeedapp/ParseApplications.kt
@@ -13,7 +13,7 @@ class ParseApplications {
         Log.d(TAG, "parse called with $xmlData")
         var status = true
         var inEntry = false
-        var testValue = ""
+        var textValue = ""
 
         try {
             val factory = XmlPullParserFactory.newInstance()
@@ -23,8 +23,47 @@ class ParseApplications {
             var currentRecord = FeedEntry()
             var eventType = xpp.eventType
             while (eventType != XmlPullParser.END_DOCUMENT) {
+                val tagName = xpp.name?.toLowerCase()    //safe-call operator ?
+                when (eventType) {
 
+                    XmlPullParser.START_TAG -> {
+                        Log.d(TAG, "parse: Starting tag for " + tagName)
+                        if (tagName == "entry") {
+                            inEntry = true
+                        }
+                    }
+
+                    XmlPullParser.TEXT -> textValue = xpp.text
+
+                    XmlPullParser.END_TAG -> {
+                        Log.d(TAG, "parse: Ending tag for " + tagName)
+                        if (inEntry) {
+                            when (tagName) {
+                                "entry" -> {
+                                    applications.add(currentRecord)
+                                    inEntry = false
+                                    currentRecord = FeedEntry()
+                                }
+
+                                "name" -> currentRecord.name = textValue
+                                "artist" ->currentRecord.artist = textValue
+                                "releasedate" -> currentRecord.releaseDate = textValue
+                                "summary" -> currentRecord.summary = textValue
+                                "image" -> currentRecord.imageURL = textValue
+                            }
+                        }
+                    }
+                }
+
+                //nothing else to do
+                eventType = xpp.next()
             }
+
+            for (app in applications) {
+                Log.d(TAG, "**************************")
+                Log.d(TAG, app.toString())
+            }
+
         } catch (e: Exception) {
             e.printStackTrace()
             status = false
